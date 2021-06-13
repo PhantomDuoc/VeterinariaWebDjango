94e654340378d4e8796e4978140f429ad902bd2a
PhantomDuoc <hu.cerdaj@alumnos.duoc.cl>
2021-06-13 19:53:30 -0400

    add urls

--- VeterinariaWeb/adopciones/views.py
+++ VeterinariaWeb/adopciones/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'formulario/index.html')
+    return render(request, 'adopciones/index.html')
