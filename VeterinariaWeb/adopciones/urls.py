7a611ddbd449b53e4b3787f35d174d095cc1eb1d
PhantomDuoc <hu.cerdaj@alumnos.duoc.cl>
2021-06-13 19:55:56 -0400

    resto htmls al urls

--- VeterinariaWeb/adopciones/urls.py
+++ VeterinariaWeb/adopciones/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import *
 
 urlpatterns = [
     path('', home, name="home"),
+    path('', contact, name="contact"),
+    path('', conocenos, name="conocenos"),
 ]
--- VeterinariaWeb/adopciones/views.py
+++ VeterinariaWeb/adopciones/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'adopciones/index.html')
+
+def contact(request):
+    return render(request, 'adopciones/adopciones.html')
+
+def conocenos(request):
+    return render(request, 'adopciones/conocenos.html')
+
