358061805de128e8e06c347a1faf0f158d1a5ef1
PhantomDuoc <hu.cerdaj@alumnos.duoc.cl>
2021-06-13 22:19:53 -0400

    /

--- VeterinariaWeb/adopciones/models.py
+++ VeterinariaWeb/adopciones/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
 # Create your models here.
-class Usuarios(models.Model):
-    idCategoria = models.IntegerField(primary_key=True, verbose_name='ID de Usuario')
-    nombreUsuario = models.CharField(max_length=50, verbose_name='Nombre de Usuario')
-    passwordUsuario = models.CharField(max_length=50, verbose_name='Contraseña')
 
-    def __str__(self) -> str:
-        return self.nombreUsuario
+class TipoUsuario(models.Model):
+    idCategoria = models.IntegerField(primary_key=True, verbose_name='Tipo de Usuario')
+    nombreCategoria = models.CharField(max_length=50, verbose_name='Nombre del tipo de usuario')
+
+    def __str__(self):
+        return self.nombreCategoria
+
+
+class Usuarios(models.Model):
+    idUsuario = models.IntegerField(primary_key=True, verbose_name='ID de Usuario')
+    nombreUsuario = models.CharField(max_length=50, verbose_name='Nombre de Usuario')
+    passwordUsuario = models.CharField(max_length=50, verbose_name='Contraseña', min_length=8)
+    categoria = models.ForeignKey(TipoUsuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.idUsuario, self.nombreUsuario
