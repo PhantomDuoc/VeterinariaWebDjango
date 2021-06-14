847abbbf2c65419fa6db6d0d0a780d9f2fed7765
PhantomDuoc <hu.cerdaj@alumnos.duoc.cl>
2021-06-13 22:08:57 -0400

    /

--- VeterinariaWeb/adopciones/models.py
+++ VeterinariaWeb/adopciones/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Usuarios(models.Model):
+    idCategoria = models.IntegerField(primary_key=True, verbose_name='ID de Usuario')
+    nombreUsuario = models.CharField(max_length=50, verbose_name='Nombre de Usuario')
+    passwordUsuario = models.CharField(max_length=50, verbose_name='Contraseña')
+
+    def __str__(self) -> str:
+        return self.nombreUsuario
