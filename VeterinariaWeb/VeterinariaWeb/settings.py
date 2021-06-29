06452b03258748f5527fb0bee1c1db21d0d62f39
Hugo Cerda <hu.cerdaj@almunos.duoc.cl>
2021-06-29 15:32:45 -0400

    jijiji

--- VeterinariaWeb/VeterinariaWeb/settings.py
+++ VeterinariaWeb/VeterinariaWeb/settings.py
@@ -77,9 +77,9 @@ WSGI_APPLICATION = 'VeterinariaWeb.wsgi.application'
 DATABASES = {
 'default': {
     'ENGINE': 'django.db.backends.oracle',
-    'NAME': '127.0.0.1:1521/xe',
-    'USER':'c##prueba',
-    'PASSWORD': 'prueba',
+    'NAME': '127.0.0.1:1521/xepdb1',
+    'USER':'django_user',
+    'PASSWORD': 'django',
     'TEST':{
         'USER':'default_test',
         'TBLSPACE':'default_test_tbls',
