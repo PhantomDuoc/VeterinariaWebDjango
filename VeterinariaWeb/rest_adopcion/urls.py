01e71491cd94c7a82af0e2a6cd3c5e80e17f3abd
PhantomDuoc <hu.cerdaj@alumnos.duoc.cl>
2021-07-04 22:00:44 -0400

    -

--- VeterinariaWeb/rest_adopcion/urls.py
+++ VeterinariaWeb/rest_adopcion/urls.py
@@ -3,4 +3,5 @@ from .views import lista_adopciones
 
 urlpatterns = [
     path('lista_adopciones', lista_adopciones, name="lista_adopciones"),
+     path('detalle-adopciones/<id>',detalle_adopciones,name='detalle_adopciones'),
 ]
--- VeterinariaWeb/rest_adopcion/views.py
+++ VeterinariaWeb/rest_adopcion/views.py
@@ -25,3 +25,24 @@ def lista_adopciones(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])
+def detalle_vehiculo(request,id):
+    try:
+        vehiculo = enAdopcion.objects.get(patente=id)
+    except enAdopcion.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = AdopcionSerializer(vehiculo)
+        return Response(serializer.data)
+    if request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = AdopcionSerializer(vehiculo,data=data)
+        if(serializer.is_valid()):
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        vehiculo.delete()
+        return Response(status=status.HTTP_204_NOT_CONTENT)
